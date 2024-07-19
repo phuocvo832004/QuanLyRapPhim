4622c1a43af6bd749920dea1187c9f76e0ced356
Phuoc Vo <voroberrt@gmail.com>
2024-07-19 12:19:19 +0700

    update services

---
+++ user-service/src/test/java/com/quanlyrapphim/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanlyrapphim.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
